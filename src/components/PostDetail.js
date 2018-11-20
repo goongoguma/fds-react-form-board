0afa11a3e641ecc2bacc7e6400bf019c65a5c397
goongoguma <wogus7an@gmail.com>
2018-11-20 15:42:20 +0900

    게시물 내용 표시기능

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
+import api from "../api";
 
 export default class PostDetail extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      body: '',
+      title: ''
+    }
+  }
+
+  // async componentDidMount(postId) {
+  //   const res = await api.get(`/posts/${postId}`)
+  //   this.setState({
+  //    body: res.data
+  //   })
+  // }
+  async componentDidMount(postId) {
+    const {data: {title, body}} = await api.get(`/posts/${this.props.postId}`);
+    this.setState({
+      title,
+      body
+    })
+  }
+
   render() {
     const{postId} = this.props
-
+    const{title, body} = this.state
     return (
       <div>
         <h1>{postId}</h1>
+        <div>{title}</div>
+        <div>{body}</div>
       </div>
     )
   }
