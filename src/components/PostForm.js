a79ad62194b023f113dc040943bb8d3fcd63a355
goongoguma <wogus7an@gmail.com>
2018-11-26 14:08:16 +0900

    PostFrom CSS 모듈 작성

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styles from './PostForm.module.scss'
 
 // 중복되기 때문에 render를 따로 빼내고 submit의 내용을 위에서 결정하게 만들었음
 // defaultValue에 다른 값을 또 넣어주지 않도록 주의
@@ -7,7 +8,7 @@ export default class PostForm extends Component {
     return ( 
       <div>
         <form onSubmit={e => this.props.onSubmit(e)}>
-          <input type="text" name="title" defaultValue={this.props.title}/>
+          <input className={styles.titleInput}type="text" name="title" defaultValue={this.props.title}/>
           <textarea name="body" cols="30" rows="10" defaultValue={this.props.body}></textarea>
           <button>전송</button>
         </form>
